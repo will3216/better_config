e6c98b0631403aa7cb9aa1374cf2769351042569
Will Bryant <william@tout.com>
2019-02-20 12:44:10 -0800

    initial commit

---
+++ lib/better_config.rb
@@ -0,0 +1,5 @@
+require "better_config/version"
+
+module BetterConfig
+  # Your code goes here...
+end
---
+++ spec/better_config_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BetterConfig do
+  it "has a version number" do
+    expect(BetterConfig::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
